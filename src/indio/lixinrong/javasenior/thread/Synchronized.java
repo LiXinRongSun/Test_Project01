a5196fe29a4ea6dc8b63cd03949daf6c6b41bb69
李新荣 <2423557714@qq.com>
2021-10-23 14:43:30 +0800

    synchronized需要等待

--- src/indio/lixinrong/javasenior/thread/Synchronized.java
+++ src/indio/lixinrong/javasenior/thread/Synchronized.java
@@ -1,9 +1,9 @@
 package indio.lixinrong.javasenior.thread;
 /**
- * 关于锁的面试题（1）
- * 不需要等待的情况
+ * 关于锁的面试题（2）
+ * 需要等待的情况
  *线程一 在完成 方法一时拿走了共享对象的锁；
- *线程二 在完成 方法二时不需要共享对象的锁所以一线程t2不需要等待；
+ *线程二 在完成 方法二时需要共享对象的锁所以线程t2需要等待；
  */
 public class Synchronized implements Runnable  {
     private Object obj;
@@ -27,8 +27,9 @@ public class Synchronized implements Runnable  {
         }
         //Java 代码自上而下逐行执行 所以if条件 是为了 t2 不用执行 方法一
         if(Thread.currentThread().getName().equals("t2")){
+        synchronized(obj){
         System.out.println(Thread.currentThread().getName()+"完成了方法二");
-        }
+        }}
     }
     public static void main(String[] args) {
         Object obj = new Object();
@@ -36,8 +37,9 @@ public class Synchronized implements Runnable  {
         Thread t2 = new Thread(new Synchronized(obj));
         t1.setName("t1");
         t2.setName("t2");
-        t1.start();
+        t1.setPriority(10);
         t2.start();
+        t1.start();   
     }
     
 }
