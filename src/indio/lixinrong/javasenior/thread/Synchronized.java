1d1e27f6e913ecbbc849ffe10acffbf961109255
李新荣 <2423557714@qq.com>
2021-10-23 17:08:11 +0800

    synchronized  类锁需要等待

--- src/indio/lixinrong/javasenior/thread/Synchronized.java
+++ src/indio/lixinrong/javasenior/thread/Synchronized.java
@@ -2,8 +2,9 @@ package indio.lixinrong.javasenior.thread;
 /**
  * 关于锁的面试题（3）
  * 需要等待的情况  类锁
- * obj 是静态的  Synchronized  的 Object  存在方法区里
- *线程一和线程2 还是共享了它 这里还是共享了对象
+ *t1 和 t2  是两个 静态方法，用 synchronized  修饰后成了类锁；
+ 虽然t1 t2  用两个对象  但是  一个拿走 类锁时另一个还是需要等待·
+ *
  *
  */
 public class Synchronized implements Runnable  {
@@ -22,41 +23,54 @@ public class Synchronized implements Runnable  {
     public  Synchronized(Object obj) {
         this.obj = obj;
     }
-    @Override
-    public void run() {
-        // TODO Auto-generated method stub
-    if(Thread.currentThread().getName().equals("t1")){
-    synchronized (obj){ //这里用this 表示这个线程
-        try {
-            Thread.sleep(1000*2);
+    //静态方法1
+    public  synchronized static void t1() {
+        System.out.println("开始方法一");
+         try {
+            Thread.sleep(1000*5);
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        System.out.println(Synchronized.getObj());
-        System.out.println( Thread.currentThread().getName()+"完成了方法一");
+        System.out.println( Thread.currentThread().getName()+"完成了方法一");   
     }
+    //静态方法2
+    public synchronized static void t2() {
+        System.out.println("开始了方法二");
+        try {
+            Thread.sleep(1000*5);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName()+"完成了方法二");
+       
+    }
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+    if(Thread.currentThread().getName().equals("t1")){
+        this.t1();
+    
+        
+    
 
         }
         //Java 代码自上而下逐行执行 所以if条件 是为了 t2 不用执行 方法一
-        if(Thread.currentThread().getName().equals("t2")){
-        synchronized(obj){
-            System.out.println(Synchronized.getObj());
-        System.out.println(Thread.currentThread().getName()+"完成了方法二");
-        }}
+    if(Thread.currentThread().getName().equals("t2")){
+        this.t2();
+       }
     }
     public static void main(String[] args) {
         Object obj = new Object();
         Object obj2 = new Object();
-        Synchronized s = new Synchronized(obj);
+        
         Thread t1 = new Thread(new Synchronized(obj));
         Thread t2 = new Thread(new Synchronized(obj2));
-        System.out.println(Synchronized.getObj());
+       
         t1.setName("t1");
         t2.setName("t2");
-        System.out.println(obj);
-        System.out.println(obj2);
-         
+        
         t1.start(); 
         t2.start(); 
     }
